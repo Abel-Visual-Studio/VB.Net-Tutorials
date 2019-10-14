76804dfe81d2b1376a02a977839b590e6b913f54
Tyler Abel <lcpdpolice4939@gmail.com>
2019-10-14 19:53:54 -0400

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Admin Login
+        If TextBox1.Text = "admin" And MaskedTextBox1.Text = "Password" Then
+            Home.Show()
+            Me.Close()
+        Else
+            MessageBox.Show("Invalid Login Information", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+End Class
